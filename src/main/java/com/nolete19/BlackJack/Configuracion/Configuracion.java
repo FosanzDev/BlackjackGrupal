d5e4a59d01d11ccb66315469688f8bdd08146538
Ram$i?_721 <71392978+Raii1234@users.noreply.github.com>
2023-06-03 20:55:33 +0100

    completó la clase Configuracion con los métodos setters para permitir cambiar los valores de cada atributo

--- src/main/java/com/nolete19/BlackJack/Configuracion/Configuracion.java
+++ src/main/java/com/nolete19/BlackJack/Configuracion/Configuracion.java
@@ -1,6 +1,6 @@
 package com.nolete19.BlackJack.Configuracion;
 
-public class Configuracion{
+public class Configuracion {
     public int numeroJugadoresIA = 3;
     public int numeroJugadoresHumanos = 1;
     public int apuestaMinima = 10;
@@ -10,4 +10,41 @@ public class Configuracion{
     public int milisegundosEspera = 1000;
     public double multiplicadorBlackjack = 1.5;
     public double multiplicadorGanadorBasico = 1;
+
+    public void setNumeroJugadoresIA(int numeroJugadoresIA) {
+        this.numeroJugadoresIA = numeroJugadoresIA;
+    }
+
+    public void setNumeroJugadoresHumanos(int numeroJugadoresHumanos) {
+        this.numeroJugadoresHumanos = numeroJugadoresHumanos;
+    }
+
+    public void setApuestaMinima(int apuestaMinima) {
+        this.apuestaMinima = apuestaMinima;
+    }
+
+    public void setApuestaMaxima(int apuestaMaxima) {
+        this.apuestaMaxima = apuestaMaxima;
+    }
+
+    public void setSaldoInicialJugadoresIA(int saldoInicialJugadoresIA) {
+        this.saldoInicialJugadoresIA = saldoInicialJugadoresIA;
+    }
+
+    public void setSaldoInicialJugadoresHumanos(int saldoInicialJugadoresHumanos) {
+        this.saldoInicialJugadoresHumanos = saldoInicialJugadoresHumanos;
+    }
+
+    public void setMilisegundosEspera(int milisegundosEspera) {
+        this.milisegundosEspera = milisegundosEspera;
+    }
+
+    public void setMultiplicadorBlackjack(double multiplicadorBlackjack) {
+        this.multiplicadorBlackjack = multiplicadorBlackjack;
+    }
+
+    public void setMultiplicadorGanadorBasico(double multiplicadorGanadorBasico) {
+        this.multiplicadorGanadorBasico = multiplicadorGanadorBasico;
+    }
 }
+
