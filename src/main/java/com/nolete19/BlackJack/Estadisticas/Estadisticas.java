e62382a5929a2052c203b007d4a63b347b3e865a
Ram$i?_721 <71392978+Raii1234@users.noreply.github.com>
2023-06-03 21:00:12 +0100

    se ha agregado una variable blackjacksJugador para registrar el número de Blackjacks obtenidos por los jugadores humanos. También se ha añadido un método incrementarBlackjacksJugador() para incrementar el contador de Blackjacks cuando un jugador obtenga uno.

--- src/main/java/com/nolete19/BlackJack/Estadisticas/Estadisticas.java
+++ src/main/java/com/nolete19/BlackJack/Estadisticas/Estadisticas.java
@@ -5,6 +5,7 @@ public class Estadisticas {
     //Definición de las variables
     private int victoriasCpu;
     private int victoriasJugador;
+    private int blackjacksJugador;
 
     /**
      * Este método se encarga de incrementar las victorias, es decir
@@ -21,6 +22,9 @@ public class Estadisticas {
         victoriasJugador++;
     }
 
+    public void incrementarBlackjacksJugador() {
+        blackjacksJugador++;
+    }
     //Getters
     public int getVictoriasCpu() {
         return victoriasCpu;
@@ -30,20 +34,23 @@ public class Estadisticas {
         return victoriasJugador;
     }
 
+    public int getBlackjacksJugador() {
+        return blackjacksJugador;
+    }
     /**
      * Método que muestra todas las estadisticas de los dos
      * jugadores al completo, para poder visualizar la diferencia.
      */
 
     public void mostrarEstadisticas() {
-
         System.out.println("============================================");
         System.out.println("                ESTADISTICAS                ");
         System.out.println("============================================");
-        System.out.println(" Manos ganadas por la CPU: " + victoriasCpu);
-        System.out.println(" Manos ganadas por el Jugador: " + victoriasJugador);
+        System.out.println("Manos ganadas por la CPU: " + victoriasCpu);
+        System.out.println("Manos ganadas por el Jugador: " + victoriasJugador);
+        System.out.println("Blackjacks obtenidos por el Jugador: " + blackjacksJugador);
+        System.out.println("Partidas jugadas: " + (victoriasCpu + victoriasJugador));
         System.out.println("============================================");
-
     }
 
 }
