1e06c2460d369681d0f0c19eb48cd5423ac56884
Esteban Sánchez <108540114+FosanzDev@users.noreply.github.com>
2023-06-04 20:48:49 +0200

    Added the png cards to the repo and modified Carta constructor

--- src/main/java/com/fosanzdev/BlackJack/Cartas/Carta.java
+++ src/main/java/com/fosanzdev/BlackJack/Cartas/Carta.java
@@ -1,15 +1,21 @@
 package com.fosanzdev.BlackJack.Cartas;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
 public class Carta {
     //Atributos enumerados
     private Palos palo;
     private Valores valor;
+    private ImageIcon image;
 
 
     //Constructor
     public Carta(Palos palo, Valores valor) {
         this.palo = palo;
         this.valor = valor;
+        this.image = new ImageIcon(this.toPath());
     }
 
     //Getters
@@ -20,11 +26,41 @@ public class Carta {
         return palo;
     }
 
+    public Image getImage() {
+        return image.getImage();
+    }
+
+    public ImageIcon getImageIcon() {
+        return image;
+    }
+
     @Override
     public String toString() {
         return "Carta{" +
                 "palo=" + palo +
                 ", valor=" + valor +
                 '}';
+    }
+
+    public String toPath(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("src/main/java/com/nolete19/BlackJack/UI/Resources/PNGCards/");
+        sb.append(this.getValor().toString());
+        switch (this.getPalo()){
+            case CORAZONES:
+                sb.append("C");
+                break;
+            case DIAMANTES:
+                sb.append("D");
+                break;
+            case PICAS:
+                sb.append("P");
+                break;
+            case TREBOL:
+                sb.append("T");
+                break;
+        }
+        sb.append(".png");
+        return sb.toString();
     }
 }
