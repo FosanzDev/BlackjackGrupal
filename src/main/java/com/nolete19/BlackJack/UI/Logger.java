1b7a0d5be04433103741fac8091fffa1427baac5
Esteban Sánchez <108540114+FosanzDev@users.noreply.github.com>
2023-06-05 11:30:46 +0200

    Modified MainFrame to be compatible with new Logger and Structure

--- src/main/java/com/nolete19/BlackJack/UI/Logger.java
+++ src/main/java/com/nolete19/BlackJack/UI/Logger.java
@@ -1,27 +1,49 @@
 package com.nolete19.BlackJack.UI;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class Logger extends JPanel{
+    String text = "";
 
-    private JTextArea textArea;
+    public Logger(Dimension size){
+        super();
+        this.setPreferredSize(size);
+    }
 
-    public Logger(){
-        super(new BorderLayout());
-        textArea = new JTextArea();
-        textArea.setEditable(false);
-        this.add(textArea, BorderLayout.CENTER);
+    public void updateSize(){
+        this.setSize(getPreferredSize()); //Set size of panel
+        repaint();
     }
 
     public void log(String message){
-        String preText = textArea.getText();
-        textArea.setText("");
-        textArea.append(message + "\n" + preText);
-        if (textArea.getText().length() > 300){
-            textArea.setText(textArea.getText().substring(0, 300));
+        String preText = text;
+        text = "";
+        text += (message + "\n" + preText);
+        if (text.length() > 300){
+            text = text.substring(0, 300);
         }
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics g2d = (Graphics) g;
+
+        //Paint the textArea
+        paintTextArea(g2d);
+        updateSize();
+    }
+
+    private void paintTextArea(Graphics g){
+        JTextArea textArea = new JTextArea();
+        textArea.setText(text);
+        textArea.setEditable(false);
+        textArea.setSize(getPreferredSize());
+        textArea.paint(g);
     }
 }
--- src/main/java/com/nolete19/BlackJack/UI/MainFrame.java
+++ src/main/java/com/nolete19/BlackJack/UI/MainFrame.java
@@ -1,11 +1,19 @@
 package com.nolete19.BlackJack.UI;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-public class MainFrame extends JFrame{
+import com.nolete19.BlackJack.Cartas.Baraja;
+import com.nolete19.BlackJack.Jugadores.JugadorHumano;
+import com.nolete19.BlackJack.Jugadores.JugadorIA;
+
+public class MainFrame extends JFrame implements Runnable{
 
     private static Logger logger;
     private static GamePanel gamePanel;
@@ -21,24 +29,76 @@ public class MainFrame extends JFrame{
         ImageIcon image = new ImageIcon("src/main/java/com/nolete19/BlackJack/UI/Resources/logo.jpg");
         this.setIconImage(image.getImage());
 
+        int xSize = this.getSize().width;
+        int ySize = this.getSize().height;
 
-        logger = new Logger();
+        System.out.println("xSize: " + xSize + " ySize: " + ySize);
+        this.setBackground(new Color(0,0,0));
+
+
+        logger = new Logger(new Dimension(xSize, ySize/4));
         this.add(logger, BorderLayout.SOUTH);
-        logger.setSize(getPreferredSize());
 
         // gamePanel = new GamePanel();
         // this.add(gamePanel, BorderLayout.CENTER);
         // gamePanel.setSize(getPreferredSize()); //Set size of panel
-
-        
-
-        log("Main frame created");
-        log("Main frame created");
-        log("Main frame created");
-        log("Main frame created");
     }
 
     public void log(String message){
         logger.log(message);
+    }
+
+
+    public static void main(String[] args) {
+        Baraja baraja = new Baraja();
+        JugadorHumano jugador = new JugadorHumano("Esteban", 2000);
+        JugadorIA jugador2 = new JugadorIA("Jugador 2", 2000);
+        JugadorIA crupier = new JugadorIA("Crupier", 2000);
+        jugador.addCarta(baraja.sacarCartaPila());
+        jugador.addCarta(baraja.sacarCartaPila());
+        jugador2.addCarta(baraja.sacarCartaPila());
+        jugador2.addCarta(baraja.sacarCartaPila());
+        jugador.addCarta(baraja.sacarCartaPila());
+        crupier.addCarta(baraja.sacarCartaPila());
+        crupier.addCarta(baraja.sacarCartaPila());
+
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.log("Main frame created");
+        mainFrame.log("Jugador: " + jugador.toString());
+        mainFrame.log("Jugador 2: " + jugador2.toString());
+        mainFrame.log("Crupier: " + crupier.toString());
+
+        
+        GameMoment gameMoment = new GameMoment(jugador, crupier, false, mainFrame.getSize());
+        GameMoment gameMoment2 = new GameMoment(jugador2, crupier, false, mainFrame.getSize());
+
+        mainFrame.setVisible(true);
+
+        Thread gameThread = new Thread(mainFrame);
+        gameThread.start();
+    }
+
+    public void setLoggerSize(){
+        logger.setPreferredSize(new Dimension(getSize().width, getSize().height/4));
+    }
+
+    public void repaint(){
+        super.repaint();
+        setLoggerSize();
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while(true){
+            try {
+                Thread.sleep(1000/60);
+                log ("Frame: " + i++);
+                i++;
+                repaint();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
